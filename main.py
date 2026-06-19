6e6af1d239a75ab786740b6fcc82c3abaf7944cf
Saint Gaia <agata.quadross2006@gmail.com>
2026-06-19 17:20:08 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+
+os.system('cls')
+
+class Atividades:
+
+    def __init__(self, nome_atividade, descricao, data_entrega, nota):
+        self.__nome_atividade = ""
+        self.__descricao = ""
+        self.__data_entrega = float()
+        self.__nota = float()
+
+    def lancar_atividade():
+
+    def calcular_data_entrega():
+
+    def exibir_atividade():

7ee7bf52dcc2b13420a3c4549734b645621097be
Saint Gaia <agata.quadross2006@gmail.com>
2026-06-22 14:25:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 import os
-
-os.system('cls')
+from datetime import date
 
 class Atividades:
 
     def __init__(self, nome_atividade, descricao, data_entrega, nota):
-        self.__nome_atividade = ""
-        self.__descricao = ""
-        self.__data_entrega = float()
-        self.__nota = float()
+        self.__nome_atividade = nome_atividade
+        self.__descricao = descricao
+        self.__data_entrega = data_entrega
+        self.__nota = nota
 
-    def lancar_atividade():
+    def lancar_atividade(self):
+        lanca = input(string("Deseja enviar essa atividade?(s/n): "))
+        
+        if lanca == "s" or lanca == "S":
+            print("Enviando atividade...")
+        elif lanca == "n" or lanca == "N":
+            print("Ok! descartando atividade")
+        else:
+            print("Opção invalida, assumindo resposta positiva")
 
-    def calcular_data_entrega():
+    def exibir_atividade(self):
+print("{self.__nome_atividade} - {self.__data_entrega}")
+        print("{self.__nota}")
+        print("{self.__descricao}")
 
-    def exibir_atividade():
+
+os.system('cls')
+
+nome = Atividades.nome_atividade(input(string("Nome da sua atividade: "))
+descricao = Atividades.descricao(input(string("descreva a sua atividade(instruções, prompt, etc): "))
+nota = Atividades.nota(input(float("quanto vale essa atividade? "))
+data = Atividades.data_entrega(input(float("qual a data de entrega? "))
+

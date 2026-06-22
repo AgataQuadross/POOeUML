133e5f53cd5074791750eee8b5ac489d53b588f8
Saint Gaia <agata.quadross2006@gmail.com>
2026-06-22 15:00:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,25 +10,35 @@ class Atividades:
         self.__nota = nota
 
     def lancar_atividade(self):
-        lanca = input(string("Deseja enviar essa atividade?(s/n): "))
+        lanca = self
         
         if lanca == "s" or lanca == "S":
             print("Enviando atividade...")
         elif lanca == "n" or lanca == "N":
             print("Ok! descartando atividade")
+            exit()
         else:
             print("Opção invalida, assumindo resposta positiva")
-
     def exibir_atividade(self):
-print("{self.__nome_atividade} - {self.__data_entrega}")
-        print("{self.__nota}")
-        print("{self.__descricao}")
+        print("~*" * 20)
+        print(f"{self.__nome_atividade} - {self.__data_entrega}")
+        print(f"{self.__nota}")
+        print("*-" * 10)
+        print(f"{self.__descricao}")
+        print("~*" * 20)
 
 
 os.system('cls')
 
-nome = Atividades.nome_atividade(input(string("Nome da sua atividade: "))
-descricao = Atividades.descricao(input(string("descreva a sua atividade(instruções, prompt, etc): "))
-nota = Atividades.nota(input(float("quanto vale essa atividade? "))
-data = Atividades.data_entrega(input(float("qual a data de entrega? "))
+nome = str(input("Nome da sua atividade: "))
+descricao = str(input("descreva a sua atividade(instruções, prompt, etc): "))
+nota = float(input("quanto vale essa atividade? "))
+data = str(input("qual a data de entrega? "))
 
+informacoes = Atividades(nome, descricao, data, nota)
+
+resposta = str(input("Deseja enviar essa atividade?(s/n): "))
+
+Atividades.lancar_atividade(resposta)
+
+Atividades.exibir_atividade(informacoes)
